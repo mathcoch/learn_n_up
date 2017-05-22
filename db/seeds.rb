bbf2779aab05d1dc999fc27afae6529f2d38879f
mathcoch <mathieu@cochet.me>
2017-05-22 15:07:23 +0200

    Faker gem added and seed done for User and Lesson

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 10 fake users...'
+10.times do
+  user = User.create(
+    email:    "#{Faker::Artist.name}@gmail.com",
+    password: "123456",
+    password_confirmation: "123456",
+    first_name: "#{Faker::Artist.name}",
+    last_name: "#{Faker::Artist.name}"
+  )
+end
+puts 'Finished!'
+
+
+puts 'Creating 10 fake lessons...'
+10.times do
+  lesson = Lesson.create(
+    category:    ['Language', 'Science', 'Sport', 'Finance'].sample,
+    name: Faker::Artist.name,
+    description: "#{Faker::ChuckNorris.fact}",
+    user_id: (1..10).to_a.sample
+  )
+end
+puts 'Finished!'

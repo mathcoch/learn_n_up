d16f943dd5b41f06a07dab126af1c262689c483a
robin bencteux <robin.bencteux@gmail.com>
2017-05-22 14:44:02 +0200

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 20170522122428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "lessons", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_lessons_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 20170522122428) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "lessons", "users"
 end

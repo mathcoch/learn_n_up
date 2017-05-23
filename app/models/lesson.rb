bf81e083ad89c98c777c076002d02eac0055f101
robin bencteux <robin.bencteux@gmail.com>
2017-05-23 12:51:11 +0200

    Champs lesson, validates non terminés

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,8 +1,12 @@
 class Lesson < ApplicationRecord
   belongs_to :user
 
-  CATEGORIES = ["Music", "Language", "Science", "Culture", "Entrepreneurship", "Sport", "Finance"]
+  CATEGORIES = ['Music', 'Language', 'Science', 'Culture', 'Entrepreneurship', 'Sport', 'Finance']
+  LEVELS = ['Beginner', 'Advanced', 'Pro']
+  DURATION = (0..20)
 
   validates :name, :description, :user, presence: true
   validates :category, inclusion: CATEGORIES
+  validates :level, inclusion: LEVELS
+  validates :duration, inclusion: DURATION
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522153216) do
+ActiveRecord::Schema.define(version: 20170523104241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20170522153216) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "img_url"
+    t.string   "level"
+    t.string   "days"
+    t.integer  "duration"
+    t.string   "address"
+    t.string   "city"
     t.index ["user_id"], name: "index_lessons_on_user_id", using: :btree
   end
 

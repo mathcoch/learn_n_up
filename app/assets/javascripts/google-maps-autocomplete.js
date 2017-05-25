7d9e311791677000d3ca5de5f30f51a4f559d84f
mathcoch <mathieu@cochet.me>
2017-05-25 16:02:01 +0200

    Version avec maps et autocomp en fonction

--- app/assets/javascripts/google-maps-autocomplete.js
+++ app/assets/javascripts/google-maps-autocomplete.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   var lesson_address = $('#lesson_address').get(0);
-
   if (lesson_address) {
     var autocomplete = new google.maps.places.Autocomplete(lesson_address, { types: ['geocode'] });
     google.maps.event.addDomListener(lesson_address, 'keydown', function(e) {

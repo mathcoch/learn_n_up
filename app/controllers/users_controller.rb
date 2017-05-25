41f8f94f5e383f7c413887284722fd0fa1f83f7f
robin bencteux <robin.bencteux@gmail.com>
2017-05-25 17:33:36 +0200

    debut compter de notif

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def show
   end
+
+  private
+
+  # def notif_count
+  #   count = 0
+  #   current_user.meetings.each do |meeting|
+  #     count +=1 if (meeting.validated == false && meeting.refused == false)
+  #   end
+  #   current_user.lessons.each do |lesson|
+  #     lesson.meetings.each do |meeting|
+
+  #     end
+  #   end
+  # end
 end

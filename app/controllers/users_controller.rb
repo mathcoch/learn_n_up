cac0367348129a4314aa74234db7a0a4790dad48
robin bencteux <robin.bencteux@gmail.com>
2017-05-25 20:14:49 +0200

    method notif count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,20 @@
 class UsersController < ApplicationController
   def show
+    notif_count
   end
 
   private
 
-  # def notif_count
-  #   count = 0
-  #   current_user.meetings.each do |meeting|
-  #     count +=1 if (meeting.validated == false && meeting.refused == false)
-  #   end
-  #   current_user.lessons.each do |lesson|
-  #     lesson.meetings.each do |meeting|
-
-  #     end
-  #   end
-  # end
+  def notif_count
+    @count = 0
+    current_user.meetings.each do |meeting|
+      @count += 1 if (meeting.validated == false || meeting.refused)
+    end
+    current_user.lessons.each do |lesson|
+      lesson.meetings.each do |meeting|
+        @count += 1 if (meeting.validated == false && meeting.refused == false)
+      end
+    end
+    return @count
+  end
 end

7ee81e43e4755b423d8c1d97123008d6967aca52
robin bencteux <robin.bencteux@gmail.com>
2017-05-25 13:34:22 +0200

    add validation columns in meetings db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525093531) do
+ActiveRecord::Schema.define(version: 20170525112839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,11 @@ ActiveRecord::Schema.define(version: 20170525093531) do
   create_table "meetings", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "lesson_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "dates"
+    t.boolean  "validated",  default: false
+    t.boolean  "refused",    default: false
     t.index ["lesson_id"], name: "index_meetings_on_lesson_id", using: :btree
     t.index ["user_id"], name: "index_meetings_on_user_id", using: :btree
   end

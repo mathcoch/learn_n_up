720171ec2948d64d90fcec793551686ba535b4b0
robin bencteux <robin.bencteux@gmail.com>
2017-05-26 12:02:29 +0200

    On remet turbolinks dans application.rb qui est nécessaire pour la recherche mais est en conflit pour l'utilisation de G Map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
 //= require gmaps/google
 //= require underscore
 //= require_tree .
+
+//turbolinks est nécessaire pour la recherche mais est en conflit pour l'utilisation de G Map.
